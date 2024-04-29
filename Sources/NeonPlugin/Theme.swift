f6e7389ba9487e882c7acbfc7cf26f74279aaf61
saharpepper <>
2024-04-29 23:17:24 +0700

    01. Added dark mode support. 02. Loading colors from xcassets file. 03. A way to add more Themes either from App's Bundle or from Package's Bundle (module).

---
+++ Sources/NeonPlugin/Theme.swift
@@ -0,0 +1,49 @@
+import Cocoa
+
+public struct Theme {
+    
+    // MARK: - Props
+    let colors: Colors
+    let tokensCustomFontDict: [TokenName: NSFont]
+    
+    // MARK: - Lifecycle
+    public init(colors: Colors, tokensCustomFontDict: [TokenName: NSFont] = [:]) {
+        self.colors = colors
+        self.tokensCustomFontDict = tokensCustomFontDict
+    }
+    
+    public var defaultFont: NSFont? {
+        return tokensCustomFontDict["default"]
+    }
+    
+    public var defaultColor: NSColor? {
+        return color(forToken: "default")
+    }
+    
+    public func color(forToken tokenName: TokenName) -> NSColor? {
+        return colors.color(forToken: tokenName)
+    }
+    
+    public func font(forToken tokenName: TokenName) -> NSFont? {
+        return tokensCustomFontDict[tokenName]
+    }
+}
+
+extension Theme {
+    
+    public struct Colors {
+        
+        let bundle: Bundle
+        let name: String
+        
+        public init(bundle: Bundle, name: String) {
+            self.bundle = bundle
+            self.name = name
+        }
+        
+        func color(forToken tokenName: TokenName) -> NSColor? {
+            let colorName = "\(name)/\(tokenName)"
+            return NSColor(named: colorName, bundle: bundle)
+        }
+    }
+}
